00f26166b3f4edbd982461807a15f89e34978357
Svetlanaask <svetlanaaskaryar@gmail.com>
2022-05-15 12:44:23 -0400

    the setting of the car

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,17 @@
 package com.company;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main (String[] args) {
 
-        basicFeatures Audi=new basicFeatures();
-Audi.Color="red";
-Audi.doors="4";
-Audi.engineCapac="V8";
-Audi.headLights="blue";
-Audi.regNo="moon";
-
+        basicFeatures Audi = new basicFeatures();
+        Audi.Color = "red";
+        Audi.doors = "4";
+        Audi.engineCapac = "V8";
+        Audi.headLights = "blue";
+        Audi.regNo = "moon";
+        
+        System.out.println("welcome to my car");
+        System.out.println("kkkkkkkkkk");
+        System.out.println("the sun roof is open");
     }
 }
